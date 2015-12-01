7915e64cf90eec718870d22a0dcbd87ac1cc5bb5
pengjiayuan <286617690@qq.com>
2015-12-01 15:43:34 +0800

    a

---
+++ androidFramework/src/com/android/androidframework/ui/commodity/bean/CommodityEntity.java
@@ -0,0 +1,56 @@
+package com.android.androidframework.ui.commodity.bean;
+
+public class CommodityEntity {
+
+	public int id = 1208;
+	public int channel_id = 0;
+	public int category_id = 12;
+	public int goods_id = 379;
+	public String name = "";
+	public String title = "浩酷蓝牙运动耳机 (EPB02)";
+	public int brand_id = 40;
+	public int color_id = 715;
+	public int capacity_id = 0;
+	public int memorysize_id = 0;
+	public int network_id = 0;
+	public String brand_name = "浩酷";
+	public String color_name = "";
+	public String capacity_name = "";
+	public String memorysize_name = "";
+	public String network_name = "";
+	public int point = 0;
+	public String add_time = "2015-11-25T16:55:44.64";
+	public int sort_id = 99;
+	public double market_price = 149;
+	public double seal_price = 128;
+	public int supplier_price = 0;
+	public int wholesale_price = 0;
+	public int cost_point = 0;
+	public int cost_nfc = 0;
+	public String promotion_logo = "";
+	public String support_service = "";
+	public String configure_introduct = "";
+	public String miaoshu = "秉承时尚简约的设计理念、发烧友最爱的高端配置呈现完美音质、高清语音让沟通不再困难。";
+	public String freebie_introduct = "";
+	public int sales = 0;
+	public int comments = 0;
+	public int clicks = 0;
+	public String shelves_time = "2015-11-25T00:00:00";
+	public String pic = "/upload/201511/25/201511251655305460.jpg";
+	public String pics = "/upload/201511/25/201511251703251397.jpg;/upload/201511/25/201511251703253585.jpg;/upload/201511/25/201511251703255928.jpg;/upload/201511/25/201511251703258585.jpg;/upload/201511/25/201511251703261397.jpg;";
+	public String description = "秉承时尚简约的设计理念、发烧友最爱的高端配置呈现完美音质、高清语音让沟通不再困难。";
+	public int stock_headquarters = 100;
+	public int stock_distributor_all = 0;
+
+	@Override
+	public String toString() {
+		return "CommodityEntity [id=" + id + ", channel_id=" + channel_id + ", category_id=" + category_id + ", goods_id=" + goods_id + ", name=" + name + ", title=" + title + ", brand_id="
+				+ brand_id + ", color_id=" + color_id + ", capacity_id=" + capacity_id + ", memorysize_id=" + memorysize_id + ", network_id=" + network_id + ", brand_name=" + brand_name
+				+ ", color_name=" + color_name + ", capacity_name=" + capacity_name + ", memorysize_name=" + memorysize_name + ", network_name=" + network_name + ", point=" + point + ", add_time="
+				+ add_time + ", sort_id=" + sort_id + ", market_price=" + market_price + ", seal_price=" + seal_price + ", supplier_price=" + supplier_price + ", wholesale_price=" + wholesale_price
+				+ ", cost_point=" + cost_point + ", cost_nfc=" + cost_nfc + ", promotion_logo=" + promotion_logo + ", support_service=" + support_service + ", configure_introduct="
+				+ configure_introduct + ", miaoshu=" + miaoshu + ", freebie_introduct=" + freebie_introduct + ", sales=" + sales + ", comments=" + comments + ", clicks=" + clicks + ", shelves_time="
+				+ shelves_time + ", pic=" + pic + ", pics=" + pics + ", description=" + description + ", stock_headquarters=" + stock_headquarters + ", stock_distributor_all=" + stock_distributor_all
+				+ "]";
+	}
+}
